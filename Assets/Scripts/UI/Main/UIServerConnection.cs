9f1d696a32763354064a67f3ba5eb46398cb7c73
YS-Kang <ysck73@gmail.com>
2020-09-14 22:42:00 +0900

    ENH: 서버 연결과 관련된 UI 클래스 관리를 위한 UIServerConnection 클래스 추가

---
+++ Assets/Scripts/UI/Main/UIServerConnection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Networking;
+
+public class UIServerConnection : MonoBehaviour
+{
+    public InputField ipInput;
+    public InputField nickNameInput;
+    public InputField roomKeyField; // 게임 방을 의미하는 RoomKey.
+
+    private void Awake()
+    {
+        ipInput.text = "192.168.0.28";
+        roomKeyField.text = "Derung";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Connect()
+    {
+        ServerManager.Instance.ModuleRoomConnect.Connect(ipInput.text, 15001, nickNameInput.text, roomKeyField.text);
+    }
+}
