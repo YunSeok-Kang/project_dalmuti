28a17561e17d7bac60385d63e202e6a98d292068
YS-Kang <ysck73@gmail.com>
2020-09-14 22:40:12 +0900

    ENH: 커스텀 타입에 대한 마샬링을 위한 마샬러 클래스 추가

---
+++ Assets/Scripts/Marshaler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalmutiClient
+{
+    public class Marshaler : Nettention.Proud.Marshaler
+    {
+        // Nettention.Proud.Marshaler 안에 기본적인 Marshaler 함수가 정의되어 있음.
+        // 3D 벡터 타입을 위한 Marshaler 함수를 추가한다.
+        public static void Write(Nettention.Proud.Message msg, UnityEngine.Vector3 b)
+        {
+            msg.Write(b.x);
+            msg.Write(b.y);
+            msg.Write(b.z);
+        }
+
+        public static void Read(Nettention.Proud.Message msg, out UnityEngine.Vector3 b)
+        {
+            b = new UnityEngine.Vector3();
+            msg.Read(out b.x);
+            msg.Read(out b.y);
+            msg.Read(out b.z);
+        }
+    }
+}
