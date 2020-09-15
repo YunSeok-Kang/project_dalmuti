a0c464dbef0ef83ba0c1404fe43fd89d62e614e1
YS-Kang <ysck73@gmail.com>
2020-09-15 19:02:25 +0900

    ENH: SMRoomConnect 모듈에 마지막으로 접속 시도했을 때의 정보를 가지고 있도록 함.
    - ConnectionInfo 클래스. 이를 LastConnectionInfo 라는 프로퍼티로 다룸.

--- Assets/Scripts/Server/Modules/SMRoomConnect.cs
+++ Assets/Scripts/Server/Modules/SMRoomConnect.cs
@@ -7,13 +7,25 @@ namespace Networking
 {
     public class SMRoomConnect : ServerModule
     {
+        public class ConnectionInfo
+        {
+            public string nickname;
+        }
+
         public SMRoomConnect(NetClient target, CommonC2S.Proxy proxy, CommonS2C.Stub stub) : base(target, proxy, stub)
         {
             Init();
         }
 
+        public ConnectionInfo LastConnectionInfo
+        {
+            get; private set;
+        }
+
         protected override bool Init()
         {
+            LastConnectionInfo = new ConnectionInfo();
+
             S2CStub.ReplyLogon = (HostID remote, RmiContext rmiContext, int result, System.String comment) =>
             {
                 //this.groupID = (HostID)groupID;
@@ -23,6 +35,8 @@ namespace Networking
                     //m_state = State.InVille;
                     Debug.Log("서버 접속 성공");
 
+                    // 여기에 있는게 맞는지 모르겠음.
+                    // 이벤트로 얘 떨궈내자.
                     UnityEngine.SceneManagement.SceneManager.LoadScene("GameLobby");
                 }
                 else
@@ -45,6 +59,7 @@ namespace Networking
                 if (info.errorType == ErrorType.Ok)
                 {
                     //m_state = State.LoggingOn;
+                    LastConnectionInfo.nickname = nickName;
 
                     // try to join the specified ville by name given by the user.
                     C2SProxy.RequestLogon(HostID.HostID_Server, RmiContext.ReliableSend, nickName, roomName);
