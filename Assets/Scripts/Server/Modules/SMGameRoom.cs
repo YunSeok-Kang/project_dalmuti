6aa3de08f79e89b348a2932f5fb6b5c7a11fa420
YS-Kang <ysck73@gmail.com>
2020-09-15 16:10:37 +0900

    ENH: 게임 룸에서 서버와의 통신을 위한 SMGameRoom 클래스 추가

---
+++ Assets/Scripts/Server/Modules/SMGameRoom.cs
@@ -0,0 +1,32 @@
+﻿using Nettention.Proud;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Networking
+{
+    public class SMGameRoom : ServerModule
+    {
+        private GameRoomC2S.Proxy _gameRoomC2SProxy = new GameRoomC2S.Proxy();
+        private GameRoomS2C.Stub _gameRoomS2CStub = new GameRoomS2C.Stub();
+
+        public SMGameRoom(NetClient target, CommonC2S.Proxy proxy, CommonS2C.Stub stub) : base(target, proxy, stub)
+        {
+            _gameRoomS2CStub.NotifyUserConnected = (Nettention.Proud.HostID remote, Nettention.Proud.RmiContext rmiContext, String nickname) =>
+            {
+                Debug.Log("접속자: " + nickname);
+                return true;
+            };
+        }
+
+        protected override bool Init()
+        {
+
+            return true;
+        }
+    }
+}
+
+
