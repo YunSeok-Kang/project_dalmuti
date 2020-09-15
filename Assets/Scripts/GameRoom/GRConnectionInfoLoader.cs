1ee08629a4c373a7934278450017da9ee915e70c
YS-Kang <ysck73@gmail.com>
2020-09-15 19:02:59 +0900

    ENH: 게임 룸에 접속 성공 시, 접속 정보를 GameRoomManager에 넘기는 GRConnectionInfoLoader 클래스 추가

---
+++ Assets/Scripts/GameRoom/GRConnectionInfoLoader.cs
@@ -0,0 +1,29 @@
+﻿using Networking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GRConnectionInfoLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        SMRoomConnect.ConnectionInfo connectionInfo = ServerManager.Instance.ModuleRoomConnect.LastConnectionInfo;
+
+        GameRoomClient myClient = new GameRoomClient();
+        myClient.isMine = true;
+
+        GameRoomManager.Instance.AddClientInfo(connectionInfo.nickname, myClient);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
