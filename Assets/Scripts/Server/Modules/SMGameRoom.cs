9df5b3b195b907f71ca9d2cc2c0906e6cab5dd34
YS-Kang <ysck73@gmail.com>
2020-09-15 16:51:59 +0900

    ENH: SMGameRoom의 생성자에서 targetClient에 proxy, stub를 attach 및 유저 연결 종료 시, 유저가 레디 시 이벤트 호출되도록 함

--- Assets/Scripts/Server/Modules/SMGameRoom.cs
+++ Assets/Scripts/Server/Modules/SMGameRoom.cs
@@ -14,9 +14,22 @@ namespace Networking
 
         public SMGameRoom(NetClient target, CommonC2S.Proxy proxy, CommonS2C.Stub stub) : base(target, proxy, stub)
         {
+            target.AttachProxy(_gameRoomC2SProxy);
+            target.AttachStub(_gameRoomS2CStub);
+
             _gameRoomS2CStub.NotifyUserConnected = (Nettention.Proud.HostID remote, Nettention.Proud.RmiContext rmiContext, String nickname) =>
             {
                 Debug.Log("접속자: " + nickname);
+                return true;
+            };
+
+            _gameRoomS2CStub.NotifyUserDisconnected = (Nettention.Proud.HostID remote, Nettention.Proud.RmiContext rmiContext, String nickname) =>
+            {
+                return true;
+            };
+
+            _gameRoomS2CStub.NotifyUserReady = (Nettention.Proud.HostID remote, Nettention.Proud.RmiContext rmiContext, String nickname) =>
+            {
                 return true;
             };
         }
