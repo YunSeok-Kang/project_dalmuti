892738ee9eb8819c5e294c544479761ac3dcb67e
YS-Kang <ysck73@gmail.com>
2020-09-16 17:47:23 +0900

    ENH: 서버에 게임 준비 상태로의 변경을 요청하는 GRGameReadyRequester 추가

---
+++ Assets/Scripts/GameRoom/GRGameReadyRequester.cs
@@ -0,0 +1,27 @@
+﻿using Networking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GRGameReadyRequester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// GameLobby -> ReadyBtn의 OnClick() 이벤트에서 해당 함수 호출.
+    /// </summary>
+    public void RequestReady()
+    {
+        ServerManager.Instance.ModuleGameRoom.RequestReady();
+    }
+}
