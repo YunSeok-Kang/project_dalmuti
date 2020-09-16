1d9268278bb39c92c8f907a8ffd6b6eb1b8e4183
YS-Kang <ysck73@gmail.com>
2020-09-16 17:46:50 +0900

    ENH: GameRoom에서 플레이어 하나의 UI를 컨트롤하는 UIGameRoomPlayerInfo와 이들 집합을 다루는 UIGameRoomPlayerInfo 추가

---
+++ Assets/Scripts/UI/GameRoom/UIGameRoomPlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using Voxellers.ObjectPooling;
+
+public class UIGameRoomPlayerControl : MonoBehaviour
+{
+    private Dictionary<GameRoomClient, UIGameRoomPlayerInfo> _playerInfoDict = new Dictionary<GameRoomClient, UIGameRoomPlayerInfo>();
+
+    public Transform parent;
+
+    private void Awake()
+    {
+        //GameRoomManager.Instance.OnUserConnected.AddListener((client) =>
+        //{
+        //    OnUserConnect(client.nickname);
+        //});
+        GameRoomManager.Instance.OnUserConnected.AddListener(OnUserConnect);
+
+        //GameRoomManager.Instance.OnUserDisconnected.AddListener((client) =>
+        //{
+        //    OnUserDisconnect(client.nickname);
+        //});
+        GameRoomManager.Instance.OnUserDisconnected.AddListener(OnUserDisconnect);
+
+        GameRoomManager.Instance.OnUserReady.AddListener(OnUserReady);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void OnUserConnect(GameRoomClient client)
+    {
+        GameObject playerInfoObject = ObjectPoolingManager.Instance.GetObject("PlayerInfo");
+        UIGameRoomPlayerInfo playerInfo = playerInfoObject.GetComponent<UIGameRoomPlayerInfo>();
+        playerInfo.nicknameText.text = client.nickname;
+
+        playerInfoObject.transform.parent = parent;
+
+        _playerInfoDict.Add(client, playerInfo);
+    }
+
+    private void OnUserDisconnect(GameRoomClient client)
+    {
+        UIGameRoomPlayerInfo playerInfo = _playerInfoDict[client];
+        ObjectPoolingManager.Instance.ReturnObject("PlayerInfo", playerInfo.gameObject);
+    }
+
+    private void OnUserReady(GameRoomClient client)
+    {
+        UIGameRoomPlayerInfo playerInfo = _playerInfoDict[client];
+        playerInfo.readyImage.gameObject.SetActive(true);
+    }
+}
